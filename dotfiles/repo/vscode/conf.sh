3b98d47f9f59e36b1c9104a9a6fb41248a4270dd
Aubrey Taylor <aubricus@gmail.com>
2019-01-14 10:28:47 -0800

    Add vscode prefs

---
+++ dotfiles/repo/vscode/conf.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# -------------------------------------
+# VSCODE DOTFILES CONF
+# -------------------------------------
+
+
+local -a links=()
+
+local -a src=()
+
+local -a copy=()
