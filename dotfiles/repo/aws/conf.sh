1ed5cf30750e98ba8f8015b14f13407eedfcb78a
Aubrey Taylor <aubricus@gmail.com>
2019-12-06 15:25:50 -0800

    Move creds.private to link from copy, aws/confg.sh

--- dotfiles/repo/aws/conf.sh
+++ dotfiles/repo/aws/conf.sh
@@ -10,9 +10,5 @@ declare -a src=()
 
 declare -a links=(
     "config:$HOME/.aws/config"
-)
-
-# Credentials will not be versioned!
-declare -a copy=(
     "credentials.private:$HOME/.aws/credentials"
 )
