3f2064e0d47ffec6f60d30d2d434c8db668c7340
Aubrey Taylor <aubricus@gmail.com>
2022-12-13 17:36:05 -0800

    Minor updates to macos/install.sh

--- macos/install.sh
+++ macos/install.sh
@@ -2,27 +2,23 @@
 
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 # MACOS INSTALL(S)
+#
+# Example:
+#   bash macos/install.sh
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-# ----------
 # Bash Flags
-# ----------
 # -e, exit on error
 # -u, unset variable is an error
 # -o pipefail, fail pipe chain if error
 set -eu -o pipefail
 
-# ----
 # Main
-# ----
-
 main() {
     # Rosetta helps some programs run on M1 architecture
     # https://support.apple.com/en-us/HT211861
     softwareupdate --install-rosetta
 }
 
-# ---
 # Run
-# ---
 main
