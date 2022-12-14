94a44e526d269e102e6d8a9215a47aee1ce834cc
Aubrey Taylor <aubricus@gmail.com>
2022-12-13 17:51:15 -0800

    Minor updates to omzsh/install.sh

--- omzsh/install.sh
+++ omzsh/install.sh
@@ -3,24 +3,22 @@
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 # OH MY ZSH INSTALL
 # - https://ohmyz.sh/#install
+# 
+# Example:
+#   bash omzsh/install.sh
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-# ----------
 # Bash Flags
-# ----------
 # -e, exit on error
 # -u, unset variable is an error
 # -o pipefail, fail pipe chain if error
 set -eu -o pipefail
 
-# ----
 # Main
-# ----
 main() {
+    # NOTE: This will check to see if OMZSH is already installed.
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 }
 
-# ---
 # Run
-# ---
 main
