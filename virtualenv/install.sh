6af46d5606e1740ce117678200c8dc2f9a027ec3
Aubrey Taylor <aubricus@gmail.com>
2022-12-13 18:07:26 -0800

    Update virtualenv/install.sh to use functions

--- virtualenv/install.sh
+++ virtualenv/install.sh
@@ -3,26 +3,28 @@
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 # VIRTUALENV INSTALL
 # - https://virtualenv.pypa.io/en/latest/installation.html
+# 
+# Example
+#  bash virtualenv/install.sh
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-# ----------
-# Bash Flags
-# ----------
-# -e, exit on error
-# -u, unset variable is an error
-# -o pipefail, fail pipe chain if error
+# Bash flags
+#  -e, exit on error
+#  -u, unset variable is an error
+#  -o pipefail, fail pipe chain if error
+#  See: https://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html
 set -eu -o pipefail
 
-# ----
-# Main
-# ----
+# Imports
+source "$PWD/lib/functions.sh"
 
+# Main
 main() {
-    pipx install virtualenv
-    virtualenv --help
+    # NOTE: `pipx` is installed using the Brewfile.
+    # NOTE: Installer will check if already installed.
+    trace pipx install virtualenv
+    trace virtualenv --version
 }
 
-# ---
 # Run
-# ---
 main
