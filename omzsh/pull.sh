2912d8a4abaa09028b35b2841aa3bb01cc528841
Aubrey Taylor <aubricus@gmail.com>
2022-12-13 17:55:03 -0800

    Update omzsh/pull.sh to use functions

--- omzsh/pull.sh
+++ omzsh/pull.sh
@@ -1,87 +1,48 @@
 #!/usr/bin/env bash
 
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-# COPY ZHS PREFS TO $HOME
+# COPY ZHS FOM HOME TO DOTFILES
 # - https://github.com/ohmyzsh/ohmyzsh/wiki
 # - https://www.zerotohero.dev/zshell-startup-files/
+#
+# Example
+#  bash omzsh/pull.sh
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-# -e, exit on error
-# -u, unset variable is an error
-# -o pipefail, fail pipe chain if error
+# Bash flags
+#  -e, exit on error
+#  -u, unset variable is an error
+#  -o pipefail, fail pipe chain if error
+#  See: https://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html
 set -eu -o pipefail
 
-# ---------
-# Functions
-# ---------
+# Imports
+source "$PWD/lib/functions.sh"
 
-log() {
-    local LEVEL="$1"
-    local MESSAGE="$2"
-
-    if [[ "$LEVEL" = "INFO" ]]; then
-        printf "$(tput setaf 7)▸ %s$(tput sgr0)\n" "$MESSAGE"
-    fi
-    if [[ "$LEVEL" = "WARNING" ]]; then
-        printf "$(tput setaf 136)! %s$(tput sgr0)\n" "$MESSAGE"
-    fi
-    if [[ "$LEVEL" = "ERROR" ]]; then
-        printf "$(tput setaf 1)x %s$(tput sgr0)\n" "$MESSAGE"
-    fi
-    if [[ "$LEVEL" = "SUCCESS" ]]; then
-        printf "$(tput setaf 64)✓ %s$(tput sgr0)\n" "$MESSAGE"
-    fi
-}
-
-confirm() {
-    log "WARNING" "$@"
-    read -p "$(tput setaf 7)▸ Please answer: (y/n) " -n 1
-    printf "\n"
-}
-
-is_confirmed() {
-    if [[ "$REPLY" =~ ^[Yy]$ ]]; then
-        return 0
-    fi
-    return 1
-}
-
-copy() {
-    local SRC="$1"
-    local DEST="$2"
-    if [[ -f "$SRC" ]]; then
-        log "INFO" "Copying $SRC to $DEST ..."
-        cp -- "$SRC" "$DEST"
-        log "SUCCESS" "Done!"
-    else
-        log "ERROR" "$SRC does not exist."
-    fi
-}
-
-# ----
 # Main
-# ----
 main() {
-    local ZPROFILE="$PWD/omzsh/.zprofile"
+    local ZPROFILE_LOCAL="$PWD/omzsh/.zprofile"
     local ZPROFILE_HOME="$HOME/.zprofile"
-    log "WARNING" "This will overwrite: $ZPROFILE"
-    confirm "Copy $ZPROFILE_HOME to $ZPROFILE anyway?"
+    msg "WARNING" "This will overwrite: $ZPROFILE_LOCAL"
+    confirm "Copy $ZPROFILE_HOME to $ZPROFILE_LOCAL anyway?"
 
     if is_confirmed; then
-        copy $ZPROFILE_HOME $ZPROFILE
+        copy "$ZPROFILE_HOME" "$ZPROFILE_LOCAL"
+    else
+        msg "ERROR" "Ok, aborting..."
     fi
 
-    local ZSHRC="$PWD/omzsh/.zshrc"
+    local ZSHRC_LOCAL="$PWD/omzsh/.zshrc"
     local ZSHRC_HOME="$HOME/.zshrc"
-    log "WARNING" "Found existing: $ZSHRC_HOME"
-    confirm "Copy $ZSHRC_HOME to $ZSHRC anyway?"
+    msg "WARNING" "This will overwrite: $ZSHRC_LOCAL"
+    confirm "Copy $ZSHRC_HOME to $ZSHRC_LOCAL anyway?"
 
     if is_confirmed; then
-        copy $ZSHRC_HOME $ZSHRC
+        copy "$ZSHRC_HOME" "$ZSHRC_LOCAL"
+    else
+        msg "ERROR" "Ok, aborting..."
     fi
 }
 
-# ---
 # Run
-# ---
 main
