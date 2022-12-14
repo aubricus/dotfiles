12a8dfbe1090e4513598b0b23e905b93f7bf5e54
Aubrey Taylor <aubricus@gmail.com>
2022-12-13 18:03:56 -0800

    Update vim/sync.sh to use functions

--- vim/sync.sh
+++ vim/sync.sh
@@ -3,73 +3,37 @@
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 # COPY VIM PREFS TO $HOME
 # - https://www.tutorialspoint.com/vim/vim_installation_and_configuration.htm
+#
+# Example:
+#  bash vim/sync.sh
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-# -e, exit on error
-# -u, unset variable is an error
-# -o pipefail, fail pipe chain if error
+# Bash flags
+#  -e, exit on error
+#  -u, unset variable is an error
+#  -o pipefail, fail pipe chain if error
+#  See: https://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html
 set -eu -o pipefail
 
-log() {
-    local LEVEL="$1"
-    local MESSAGE="$2"
+# Imports
+source "$PWD/lib/functions.sh"
 
-    if [[ "$LEVEL" = "INFO" ]]; then
-        printf "$(tput setaf 7)▸ %s$(tput sgr0)\n" "$MESSAGE"
-    fi
-    if [[ "$LEVEL" = "WARNING" ]]; then
-        printf "$(tput setaf 136)! %s$(tput sgr0)\n" "$MESSAGE"
-    fi
-    if [[ "$LEVEL" = "ERROR" ]]; then
-        printf "$(tput setaf 1)x %s$(tput sgr0)\n" "$MESSAGE"
-    fi
-    if [[ "$LEVEL" = "SUCCESS" ]]; then
-        printf "$(tput setaf 64)✓ %s$(tput sgr0)\n" "$MESSAGE"
-    fi
-}
-
-confirm() {
-    log "WARNING" "$@"
-    read -p "$(tput setaf 7)▸ Please answer: (y/n) " -n 1
-    printf "\n"
-}
-
-is_confirmed() {
-    if [[ "$REPLY" =~ ^[Yy]$ ]]; then
-        return 0
-    fi
-    return 1
-}
-
-copy() {
-    local FILE="$@"
-    if [[ -f "$FILE" ]]; then
-        log "INFO" "Copying $FILE to $HOME ..."
-        cp -- "$FILE" "$HOME"
-        log "SUCCESS" "Done!"
-    else
-        log "ERROR" "$FILE does not exist."
-    fi
-}
-
-# ----
 # Main
-# ----
 main() {
-    local VIMRC="$PWD/vim/.vimrc"
-    local VIMRC_EXISTING="$HOME/.vimrc"
-    if [[ -e "$VIMRC_EXISTING" ]]; then
-        log "WARNING" "Found existing: $VIMRC_EXISTING"
-        confirm "Copy $VIMRC to $HOME anyway?"
+    local VIMRC_LOCAL="$PWD/vim/.vimrc"
+    local VIMRC_HOME="$HOME/.vimrc"
+    if [[ -e "$VIMRC_HOME" ]]; then
+        msg "WARNING" "Found existing: $VIMRC_HOME"
+        confirm "Copy $VIMRC_LOCAL to $VIMRC_HOME anyway?"
         if is_confirmed; then
-            copy $VIMRC
+            copy "$VIMRC_LOCAL" "$VIMRC_HOME"
+        else
+            msg "ERROR" "Ok, aborting..."
         fi
     else
-        copy $VIMRC
+        copy "$VIMRC_LOCAL" "$VIMRC_HOME"
     fi
 }
 
-# ---
 # Run
-# ---
 main
